14c9b61d8634ec18da152c3034edce519f661e45
mingyudeng <dmy1024@qq.com>
2025-09-01 16:46:54 +0800

    init

---
+++ build/Desktop_Qt_6_9_2_llvm_mingw_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_calculator_FILE D:/project/calculator/build/Desktop_Qt_6_9_2_llvm_mingw_64_bit-Debug/calculator.exe)
+set(__QT_DEPLOY_TARGET_calculator_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_calculator_RUNTIME_DLLS D:/Qt/6.9.2/llvm-mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.9.2/llvm-mingw_64/bin/Qt6Gui.dll;D:/Qt/6.9.2/llvm-mingw_64/bin/Qt6Core.dll)
